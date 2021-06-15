5f7800fa2717bc0794d967247e6f8f794e47a41b
Emerson Rocha de Oliveira <emersonrochadeoliveira@gmail.com>
2021-06-15 09:02:37 -0300

    Criando classe seguindo o Open Close Principle

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole  //Record define uma classe imutavel
+    {
+        public EmployeeRole(string id, string roleName)
+        {
+            Id = id;
+            RoleName = roleName;
+        }
+
+        public string Id { get; private set; } 
+
+        public string RoleName { get; init; } // o init garante que a propriedade seja alterada apenas no construtor e em nenhum outro método.
+    }   
+}
